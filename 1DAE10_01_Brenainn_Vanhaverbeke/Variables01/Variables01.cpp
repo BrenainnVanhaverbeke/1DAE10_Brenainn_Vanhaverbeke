4c8bf9c4e16f066d0ca8fca33144d3229bcbc509
Brenainn Vanhaverbeke <brenainnvanhaverbeke@gmail.com>
2021-10-07 15:23:17 +0200

    First commit

---
+++ 1DAE10_01_Brenainn_Vanhaverbeke/Variables01/Variables01.cpp
@@ -0,0 +1,35 @@
+// Variables01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    int number = 5;
+    char letter = 'C';
+    bool binaryValue = false;
+    float floatingPointValue = 0.1f;
+    double preciseFloatingPointValue = 0.2f;
+
+    std::cout << number;
+    std::cout << letter;
+    std::cout << binaryValue;
+    std::cout << floatingPointValue;
+    std::cout << preciseFloatingPointValue;
+    std::cout << number + 4;
+    std::cout << floatingPointValue + 0.2f;
+    std::cout << letter + 'D';
+    std::cout << number + letter + "potatomancy";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Core Framework/Game.h
@@ -0,0 +1,34 @@
+#pragma once
+using namespace utils;
+#pragma region gameInformation
+// Set your name and group in the title here
+std::string g_WindowTitle{ "Project name - Name, firstname - 1DAExx" };
+
+// Change the window dimensions here
+float g_WindowWidth{ 500 };
+float g_WindowHeight{ 300 };
+#pragma endregion gameInformation
+
+
+
+#pragma region ownDeclarations
+// Declare your own global variables here
+
+// Declare your own functions here
+
+#pragma endregion ownDeclarations
+
+#pragma region gameFunctions											
+void Start();
+void Draw();
+void Update(float elapsedSec);
+void End();
+#pragma endregion gameFunctions
+
+#pragma region inputHandling											
+void OnKeyDownEvent(SDL_Keycode key);
+void OnKeyUpEvent(SDL_Keycode key);
+void OnMouseMotionEvent(const SDL_MouseMotionEvent& e);
+void OnMouseDownEvent(const SDL_MouseButtonEvent& e);
+void OnMouseUpEvent(const SDL_MouseButtonEvent& e);
+#pragma endregion inputHandling
