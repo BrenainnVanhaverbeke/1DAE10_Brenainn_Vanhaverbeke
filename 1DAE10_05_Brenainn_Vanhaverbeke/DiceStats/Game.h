6e46f253a4a6993538b077ef8945e96c2f09c71c
Brenainn <brenainnvanhaverbeke@gmail.com>
2021-10-27 14:57:21 +0200

    Completed DiceStats

--- 1DAE10_05_Brenainn_Vanhaverbeke/DiceStats/Game.h
+++ 1DAE10_05_Brenainn_Vanhaverbeke/DiceStats/Game.h
@@ -6,16 +6,50 @@ std::string g_WindowTitle{ "Dice Stats - Vanhaverbeke, Brenainn - 1DAE10" };
 
 // Change the window dimensions here
 float g_WindowWidth{ 500 };
-float g_WindowHeight{ 300 };
+float g_WindowHeight{ 500 };
 #pragma endregion gameInformation
 
 
 
 #pragma region ownDeclarations
 // Declare your own global variables here
+const float g_DefaultBorder{ 5 };
+const float g_DefaultHeight{ 20 };
+
+const Color4f g_Blue{ 0, 0, 1, 1 };
+const Color4f g_Red{ 1, 0, 0, 1 };
+
+int g_Roll1Single{};
+int g_Roll2Single{};
+int g_Roll3Single{};
+int g_Roll4Single{};
+int g_Roll5Single{};
+int g_Roll6Single{};
+int g_Roll2{};
+int g_Roll3{};
+int g_Roll4{};
+int g_Roll5{};
+int g_Roll6{};
+int g_Roll7{};
+int g_Roll8{};
+int g_Roll9{};
+int g_Roll10{};
+int g_Roll11{};
+int g_Roll12{};
+
+
 
 // Declare your own functions here
 
+int GetDiceRoll(int min, int max);
+void IncrementSingleRollFrequencyTable(int diceRoll);
+void IncrementDoubleFrequencyTable(int diceRoll);
+int GetSingleRollFrequency(int rollNumber);
+int GetDoubleRollFrequency(int rollNumber);
+
+void DrawFrequencyBar(int number, int frequency, Color4f colour, float initialOffset = 0);
+float GetOffsetY(int number);
+Rectf GenerateBar(int number, int frequency, float initialOffset = 0);
 #pragma endregion ownDeclarations
 
 #pragma region gameFunctions											
