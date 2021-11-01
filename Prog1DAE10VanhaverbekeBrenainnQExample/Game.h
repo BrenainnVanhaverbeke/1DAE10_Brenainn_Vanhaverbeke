38d7069d5f868ab96ef5ba8b8c3788bbed584f55
Brenainn Vanhaverbeke <brenainnvanhaverbeke@gmail.com>
2021-11-01 15:00:02 +0100

    Started QExample, finished first part

---
+++ Prog1DAE10VanhaverbekeBrenainnQExample/Game.h
@@ -0,0 +1,44 @@
+#pragma once
+using namespace utils;
+#pragma region gameInformation
+// Set your name and group in the title here
+std::string g_WindowTitle{ "QExample - Vanhaverbeke, Brenainn - 1DAE10" };
+
+// Change the window dimensions here
+float g_WindowWidth{ 500 };
+float g_WindowHeight{ 300 };
+#pragma endregion gameInformation
+
+
+
+#pragma region ownDeclarations
+// Declare your own global variables here
+
+int g_Even{};
+int g_Odd{};
+int g_Any{};
+float g_TotalElapsedTime{};
+
+// Declare your own functions here
+
+void Generate();
+void PrintRandomNumbers();
+
+int GenerateRandomNumber(int min, int max);
+
+#pragma endregion ownDeclarations
+
+#pragma region gameFunctions											
+void Start();
+void Draw();
+void Update(float elapsedSec);
+void End();
+#pragma endregion gameFunctions
+
+#pragma region inputHandling											
+void OnKeyDownEvent(SDL_Keycode key);
+void OnKeyUpEvent(SDL_Keycode key);
+void OnMouseMotionEvent(const SDL_MouseMotionEvent& e);
+void OnMouseDownEvent(const SDL_MouseButtonEvent& e);
+void OnMouseUpEvent(const SDL_MouseButtonEvent& e);
+#pragma endregion inputHandling
