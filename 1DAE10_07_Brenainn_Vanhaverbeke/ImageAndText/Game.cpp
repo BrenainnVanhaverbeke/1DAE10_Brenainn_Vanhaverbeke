a86bdf157adfa1b3a0891b36c964d640c7d281df
Brenainn Vanhaverbeke <brenainnvanhaverbeke@gmail.com>
2021-11-25 22:10:46 +0100

    Completed ImageAndText

---
+++ 1DAE10_07_Brenainn_Vanhaverbeke/ImageAndText/Game.cpp
@@ -0,0 +1,104 @@
+// Vanhaverbeke, Brenainn - 1DAE10
+
+#include "pch.h"
+#include "Game.h"
+#include <iostream>
+
+//Basic game functions
+#pragma region gameFunctions											
+void Start()
+{
+	TextureFromFile("Resources/DAE.png", g_DaeTexture);
+	TextureFromString("I like potatoes", "Resources/DIN-Light.otf", 34, Color4f{ 1.0f, 1.0f, 0.0f, 1.0f }, g_Banner);
+}
+
+void Draw()
+{
+	ClearBackground(0.0f, 0.0f, 0.3f);
+	DrawTexture(g_DaeTexture, Point2f{ 10.0f, g_WindowHeight - g_DaeTexture.height - 10.0f });
+	DrawTexture(g_Banner, Point2f{ 10.0f , g_WindowHeight - g_DaeTexture.height * 2.0f });
+}
+
+void Update(float elapsedSec)
+{
+	// process input, do physics 
+
+	// e.g. Check keyboard state
+	//const Uint8 *pStates = SDL_GetKeyboardState( nullptr );
+	//if ( pStates[SDL_SCANCODE_RIGHT] )
+	//{
+	//	std::cout << "Right arrow key is down\n";
+	//}
+	//if ( pStates[SDL_SCANCODE_LEFT] && pStates[SDL_SCANCODE_UP])
+	//{
+	//	std::cout << "Left and up arrow keys are down\n";
+	//}
+}
+
+void End()
+{
+	DeleteTexture(g_DaeTexture);
+	DeleteTexture(g_Banner);
+}
+#pragma endregion gameFunctions
+
+//Keyboard and mouse input handling
+#pragma region inputHandling											
+void OnKeyDownEvent(SDL_Keycode key)
+{
+
+}
+
+void OnKeyUpEvent(SDL_Keycode key)
+{
+	//switch (key)
+	//{
+	//case SDLK_LEFT:
+	//	//std::cout << "Left arrow key released\n";
+	//	break;
+	//case SDLK_RIGHT:
+	//	//std::cout << "Right arrow key released\n";
+	//	break;
+	//case SDLK_1:
+	//case SDLK_KP_1:
+	//	//std::cout << "Key 1 released\n";
+	//	break;
+	//}
+}
+
+void OnMouseMotionEvent(const SDL_MouseMotionEvent& e)
+{
+	//std::cout << "  [" << e.x << ", " << e.y << "]\n";
+	//Point2f mousePos{ float( e.x ), float( g_WindowHeight - e.y ) };
+}
+
+void OnMouseDownEvent(const SDL_MouseButtonEvent& e)
+{
+
+}
+
+void OnMouseUpEvent(const SDL_MouseButtonEvent& e)
+{
+	////std::cout << "  [" << e.x << ", " << e.y << "]\n";
+	//switch (e.button)
+	//{
+	//case SDL_BUTTON_LEFT:
+	//{
+	//	//std::cout << "Left mouse button released\n";
+	//	//Point2f mousePos{ float( e.x ), float( g_WindowHeight - e.y ) };
+	//	break;
+	//}
+	//case SDL_BUTTON_RIGHT:
+	//	//std::cout << "Right mouse button released\n";
+	//	break;
+	//case SDL_BUTTON_MIDDLE:
+	//	//std::cout << "Middle mouse button released\n";
+	//	break;
+	//}
+}
+#pragma endregion inputHandling
+
+#pragma region ownDefinitions
+// Define your own functions here
+
+#pragma endregion ownDefinitions
