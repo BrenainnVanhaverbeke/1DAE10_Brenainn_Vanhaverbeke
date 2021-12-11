cd15eeeae20fed8ec768b442f37feb5f74cb04c6
Brenainn Vanhaverbeke <brenainnvanhaverbeke@gmail.com>
2021-12-11 20:28:39 +0100

    Completed StringBasics

--- 1DAE10_09_Brenainn_Vanhaverbeke/StringBasics/StringBasics.cpp
+++ 1DAE10_09_Brenainn_Vanhaverbeke/StringBasics/StringBasics.cpp
@@ -7,9 +7,6 @@ int main()
 	const std::string theMetamorphosis{ "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked." };
 	std::cout << "--- Original string: ---" << std::endl << std::endl;
 	std::cout << theMetamorphosis << std::endl << std::endl;
-
-	const char* pTheMetamorphosis{ theMetamorphosis.c_str() };
-
 	std::cout << "--- The Metamorphosis statistics: ---" << std::endl << std::endl;
 	DisplayStringStatistics(theMetamorphosis);
 	std::cout << "--- The Metamorphosis seperated by '_' ---" << std::endl << std::endl;
@@ -21,6 +18,12 @@ int main()
 	std::cout << "Please insert a string to find in The Metamorphosis: ";
 	std::cin >> userInput;
 	FindInString(theMetamorphosis, userInput);
+	std::cout << "Finding your string in reverse order: " << std::endl;
+	ReverseFindInString(theMetamorphosis, userInput);
+	std::cout << "Replacing your string with '*': " << std::endl;
+	ReplaceInString(theMetamorphosis, userInput, '*');
+	std::cout << "Erasing your string: " << std::endl;
+	EraseInString(theMetamorphosis, userInput);
 }
 
 void DisplayStringStatistics(const std::string& s)
@@ -38,7 +41,7 @@ void PrintStringSeperatedWithCharacter(const std::string& s, char seperator)
 {
 	const size_t stringSize{ s.size() };
 	const char* pStringChars{ s.c_str() };
-	for (int i{ 0 }; i < stringSize; ++i)
+	for (size_t i{ 0 }; i < stringSize; ++i)
 	{
 		std::cout << s[i];
 		if (i < stringSize - 1)
@@ -70,9 +73,56 @@ void FindInString(const std::string& toSearchIn, const std::string& toSearch)
 			position++;
 		}
 	} while (position != std::string::npos);
+	std::cout << std::endl << std::endl;
 }
 
 void ReverseFindInString(const std::string& toSearchIn, const std::string& toSearch)
 {
+	size_t position{ toSearchIn.size() - 1 };
+	std::cout << "Occurances of '" << toSearch << "' at: " << std::endl;
+	do
+	{
+		position = toSearchIn.rfind(toSearch, position);
+		if (position != std::string::npos)
+		{
+			std::cout << position << " ";
+			position--;
+		}
+	} while (position != std::string::npos);
+	std::cout << std::endl << std::endl;
+}
 
+void ReplaceInString(std::string toReplaceIn, const std::string& toReplace, char replacingWith)
+{
+	size_t position{};
+	std::string replacementString{};
+	for (size_t i = 0; i < toReplace.size(); i++)
+		replacementString += replacingWith;
+	do
+	{
+		position = toReplaceIn.find(toReplace, position);
+		if (position != std::string::npos)
+		{
+			toReplaceIn.replace(position, toReplace.size(), replacementString);
+			position++;
+		}
+	} while (position != std::string::npos);
+	std::cout << toReplaceIn << std::endl;
+	std::cout << std::endl;
+}
+
+void EraseInString(std::string toEraseIn, const std::string& toErase)
+{
+	size_t position{};
+	do
+	{
+		position = toEraseIn.find(toErase, position);
+		if (position != std::string::npos)
+		{
+			toEraseIn.erase(position, toErase.size());
+			position++;
+		}
+	} while (position != std::string::npos);
+	std::cout << toEraseIn << std::endl;
+	std::cout << std::endl;
 }
--- 1DAE10_09_Brenainn_Vanhaverbeke/StringBasics/pch.h
+++ 1DAE10_09_Brenainn_Vanhaverbeke/StringBasics/pch.h
@@ -8,5 +8,8 @@ void DisplayStringStatistics(const std::string& s);
 void PrintStringSeperatedWithCharacter(const std::string& s, char seperator);
 void PrintFirstAndLastLetterOfString(const std::string& s);
 void FindInString(const std::string& toSearchIn, const std::string& toSearch);
+void ReverseFindInString(const std::string& toSearchIn, const std::string& toSearch);
+void ReplaceInString(std::string toReplaceIn, const std::string& toReplace, char replacingWith);
+void EraseInString(std::string toDeleteIn, const std::string& toDelete);
 
-#endif
+#endif PCH_H
